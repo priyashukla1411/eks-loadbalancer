3e51b3e21ba2cea4001d37d1902b65668d783ce6
priyashukla1411 <100112297+priyashukla1411@users.noreply.github.com>
2023-06-02 07:04:24 +0530

    Update and rename app.js to server.js

--- app.js
+++
@@ -1,9 +0,0 @@
-const http = require('http');
-
-const server = http.createServer((req, res) => {
-  res.end('KUBERNETES +++ The deployment, scaling, and maintenance of containerized applications are all automated via the open-source container orchestration technology known as Kubernetes.');
-});
-
-server.listen(3000, () => {
-  console.log('Server listening on port 3000');
-});
